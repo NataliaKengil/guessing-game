c7b7687b4890ac8b75d74fcf8bb7116738a732c6
NataliaKengil <241283nata@gmail.com>
2024-10-03 20:27:46 +0300

    feat: test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,58 @@
 class GuessingGame {
-    constructor() {}
+    /*constructor() {
+        this.min = 0;  
+        this.max = 0;  
+        this.currentGuess = 0; // 
+    }
 
     setRange(min, max) {
-
+        this.min = min; // Устанавливаем минимальное значение диапазона
+        this.max = max; // Устанавливаем максимальное значение диапазона
     }
 
     guess() {
-
+        this.currentGuess = Math.floor((this.min + this.max) / 2); // Находим среднее значение
+        return this.currentGuess; // Возвращаем предположение
     }
 
     lower() {
-
+        this.max = this.currentGuess; // Если предположение слишком велико, сужаем диапазон
     }
 
     greater() {
+        this.min = this.currentGuess + 1; // Если предположение слишком мало, сужаем диапазон
+    }*/
 
-    }
+        
+            constructor() {
+                this.min = null;
+                this.max = null;
+                this.guessValue = null;
+            }
+        
+            setRange(min, max) {
+                this.min = min;
+                this.max = max;
+                this.guessValue = Math.floor((this.min + this.max) / 2); // Первое предположение
+            }
+        
+            guess() {
+                return this.guessValue;
+            }
+        
+            lower() {
+                this.max = this.guessValue - 1; // Уменьшаем верхнюю границу
+                this.guessValue = Math.floor((this.min + this.max) / 2); // Новое предположение
+            }
+        
+            greater() {
+                this.min = this.guessValue + 1; // Увеличиваем нижнюю границу
+                this.guessValue = Math.floor((this.min + this.max) / 2); // Новое предположение
+            }
+        
 }
 
 module.exports = GuessingGame;
+
+
+  
